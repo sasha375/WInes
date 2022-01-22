67a90330e1f5e3c2853c8881df9b6b3c5b5a0b25
sasha375 <76862627+sasha375@users.noreply.github.com>
2022-01-22 17:54:56 +0300

    using argparse

--- main.py
+++ main.py
@@ -2,44 +2,20 @@ import collections
 import datetime
 import pandas
 import sys
+import argparse
 import os
 
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 
-path_in_console = False
-sheet_name_in_path = False
-
-if len(sys.argv) >= 2:
-    if sys.argv[1] != "!inherit":
-        path = sys.argv[1]
-        path_in_console = True
-    if len(sys.argv) >= 3:
-        if sys.argv[2] != "!inherit":
-            sheet_name = sys.argv[2]
-            sheet_name_in_path = True
-
-if (not path_in_console) or (not sheet_name_in_path):
-    if os.path.exists("config.py"):
-        import config
-        if not path_in_console:
-            path = config.production_path
-        if not sheet_name_in_path:
-            sheet_name = config.sheet_name
-    else:
-        raise FileNotFoundError("""
-please specify production path or write it to config.py
-Usage:
-    python main.py [path_to_production|!inherit [sheet_name|!inherit]]
-    (!inherit = from config.py)
-config.py syntax:
-    production_path = "path/to/production.xlsx"
-    sheet_name = "Sheetname"
-""")
+parser = argparse.ArgumentParser(description='Описание что делает программа')
+parser.add_argument('--production-path', default=os.getenv("PRODUCTION_PATH"))
+parser.add_argument('--sheet-name', default=os.getenv("SHEET_NAME"))
+args = parser.parse_args()
 
 
 
-wines = pandas.read_excel(path, sheet_name=sheet_name, na_values='', keep_default_na=False).fillna('').to_dict(orient='record')
+wines = pandas.read_excel(args.production_path, sheet_name=args.sheet_name, na_values='', keep_default_na=False).fillna('').to_dict(orient='record')
 
 run_year = 1920
 
