f788ee6204d28a31aaaca7346a6e64244769dfc4
sasha375 <76862627+sasha375@users.noreply.github.com>
2022-01-23 20:41:55 +0300

    added ifmain

--- main.py
+++ main.py
@@ -64,8 +64,12 @@ rendered_page = template.render(
     categories=production
 )
 
-with open('index.html', 'w', encoding="utf8") as file:
-    file.write(rendered_page)
+def main():
+    with open('index.html', 'w', encoding="utf8") as file:
+        file.write(rendered_page)
 
-server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
-server.serve_forever()
+    server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
+    server.serve_forever()
+
+if __name__ == '__main__':
+    main()
